881f1eef2aa634c8f7d3b7a25f6c80b729270cd1
Priyam7302 <priyamvada7302@gmail.com>
2025-12-30 14:42:58 +0530

    summary quantity updated

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -59,6 +59,11 @@ const Cart = () => {
     if (!item.productId) return acc;
     return acc + item.productId.originalPrice * item.quantity;
   }, 0);
+  const totalItems = cartItems.reduce((acc, item) => {
+    if (!item.productId) return acc;
+    return acc + item.quantity;
+  }, 0);
+
 
   return (
     <section className="cart-page">
